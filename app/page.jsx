7270e12bebbb363e3eb2193f856971563ab86fb2
NoePupin <noe@diffusez.com>
2024-02-13 11:25:47 +0100

    🏗️ Showing list of users

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import Cart from "./components/server/showUser";
+
 export const metadata = {
   title: "Code And Dragons - Accueil",
 };
@@ -7,6 +9,7 @@ export default function Home() {
   return (
     <main>
       <h1>Home page!</h1>
+      <Cart />
     </main>
   );
 }
