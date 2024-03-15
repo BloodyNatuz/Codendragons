c8afc0a87fee67c91df76c4cdd7206ec2eeece01
NoePupin <noe@diffusez.com>
2024-03-15 16:19:45 +0100

    🎨 Metadatas in layout

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,7 @@
-import Cart from "./components/server/showUser";
-
-export const metadata = {
-  title: "Code And Dragons - Accueil",
-};
-
 export default function Home() {
   return (
     <main>
       <h1>Home page!</h1>
-      <Cart />
     </main>
   );
 }
