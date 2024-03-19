1fdf8a386fd9d2966a42c6359990e1d90a2195cf
NoePupin <noe@diffusez.com>
2024-03-19 15:49:28 +0100

    ✨ form to update wakatimekey

---
+++ app/components/client/inputWakatimeKey.jsx
@@ -0,0 +1,46 @@
+'use client'
+
+import { useState } from "react"
+
+export default function InputWakatimeKey(){
+    const [formData, setFormData] = useState({
+        wakatimekey: ''
+      });
+
+    const handleInputChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (formData.wakatimekey.length == null){
+            // setAppearance('appearance-error');
+            // setMessageContent('Aucune API Key rentrée');
+        } else{
+            const response = await fetch('/api/wakatimekey', {
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData)
+            })
+            // setAppearance('appearance-success');
+            // setMessageContent('Votre compte a été créé avec succès !');
+            window.location.reload();
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <fieldset>
+                <legend>Wakatime API Key</legend>
+                <input name="wakatimekey" type="text" value={formData.wakatimekey} onChange={handleInputChange}></input>
+            </fieldset>
+            <input type="submit" />
+        </form>
+    )
+}
