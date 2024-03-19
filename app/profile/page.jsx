98ef21f2c85ddc21154658bb1c73c7551b501ef1
NoePupin <noe@diffusez.com>
2024-03-19 15:50:47 +0100

    🚧 Wakatime key on profile

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -2,16 +2,28 @@
 
 import { cookies } from 'next/headers'
 import Logout from '../components/client/logout';
+import InputWakatimeKey from '../components/client/inputWakatimeKey';
+import ResetWakatimeKey from '../components/client/resetWakatimeKey';
 
 export default async function Profile(){
     const cookieStore = cookies();
     const username = cookieStore.get('username');
     const email = cookieStore.get('email');
+    const wakatime = cookieStore.get('wakatimekey');
 
     return (
         <main>
             <p>Username: {username.value}</p>
             <p>Email: {email.value}</p>
+            {
+                wakatime == undefined
+                    ? <InputWakatimeKey/>
+                    : (<>
+                        <p>Wakatime API Key: {wakatime.value}</p>
+                        <ResetWakatimeKey/>
+                    </>)
+            }
+            <br/>
             <Logout/>
         </main>
     )
