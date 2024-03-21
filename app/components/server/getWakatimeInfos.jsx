6f883eeb27d986ecab2b6086fd8d3aaa5fc362b4
NoePupin <noe@diffusez.com>
2024-03-21 13:18:33 +0100

    🚧 Algo calculate total time

--- app/components/server/getWakatimeInfos.jsx
+++ app/components/server/getWakatimeInfos.jsx
@@ -7,22 +7,24 @@ export async function GetWakatimeInfos(){
     const cookieStore = cookies();
     const wakatimekey = cookieStore.get('wakatimekey');
     const wakatimeid = cookieStore.get('wakatimeid');
-    // Gérer le Last Login (à la fin)
     
     // Calculer if temps passé > 14 alors calcule de temps total
     // let setTodayDate = new Date();
-    // let setLastLogin = new Date('01/10/2024');
+    // let setLastActive = new Date('01/10/2024');
     // let todayDate = setTodayDate.toLocaleDateString("ja-JP");
-    // let lastLogin = setLastLogin.toLocaleDateString("ja-JP");
+    // let lastActive = setLastActive.toLocaleDateString("ja-JP");
     // let setDateCompare = new Date(setTodayDate.setDate(setTodayDate.getDate() - 14));
     // let dateCompare = setDateCompare.toLocaleDateString("ja-JP");
-    // if (lastLogin < dateCompare) {
+
+
+    // if (lastActive < dateCompare) {
     //     console.log("Tu t'es connecté il y a plus de 14 jours")
-    //     setLastLogin = new Date();
+    //     setLastActive = new Date();
     //     setTodayDate = new Date();
     // } else {
     //     console.log("Tu t'es connecté il y a moins de 14 jours")
     //     setTodayDate = new Date();
+    //      setLastActive = new Date();
     // }
 
     if (wakatimekey == undefined || wakatimeid == undefined) {
